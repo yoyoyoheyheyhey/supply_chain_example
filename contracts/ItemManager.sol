4c58cbc6401f2e9672f905ad22d2613cad658646
Yohei Goto <yohei_goto@diveintocode.jp>
2020-11-23 20:06:54 +0900

    Replace Smart Contracts in the Truffle React Box

--- contracts/ItemManager.sol
+++ contracts/ItemManager.sol
@@ -1,45 +1,7 @@
-pragma solidity ^0.7.0;
+pragma solidity ^0.6.0;
 
-contract Ownable {
-    address payable _owner;
-    
-    constructor() public {
-        _owner = msg.sender;
-    }
-    
-    modifier onlyOwner() {
-        require(isOwner(), "You are not the owner");
-        _;
-    }
-    
-    function isOwner() public view returns(bool) {
-        return (msg.sender == _owner);
-    }
-}
-
-contract Item {
-    uint public priceInWei;
-    uint public pricePaid;
-    uint public index;
-    
-    ItemManager parentContract;
-    
-    constructor(ItemManager _parentContract, uint _priceInWei, uint _index) public {
-        priceInWei = _priceInWei;
-        index = _index;
-        parentContract = _parentContract;
-    }
-    
-    receive() external payable {
-        require(pricePaid == 0, "Item is paid already");
-        require(priceInWei == msg.value, "Only full payments allowed");
-        pricePaid += msg.value;
-        (bool success, ) = address(parentContract).call{value: msg.value}(abi.encodeWithSignature("triggerPayment(uint256)", index));
-        require(success, "The transaction wasn't successufl, canceling");
-    }
-    
-    fallback() external {}
-}
+import "./Ownable.sol";
+import "./Item.sol";
 
 contract ItemManager is Ownable {
     
