76380378ecad6fce9ccd0430ef2139fce169d8f9
Yohei Goto <yohei_goto@diveintocode.jp>
2020-11-23 21:45:41 +0900

    Modify the App.js and HTML Part in the React Box

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,31 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import ItemManager from "./contracts/ItemManager.json";
+import Item from "./contracts/Item.json";
 import getWeb3 from "./getWeb3";
-
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { cost: 0, itemName: "exampleItem1", loaded: false };
 
   componentDidMount = async () => {
     try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+      this.web3 = await getWeb3();
 
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+      this.accounts = await this.web3.eth.getAccounts();
 
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
+      const networkId = await this.web3.eth.net.getId();
+
+      this.itemManager = new this.web3.eth.Contract(
+        ItemManager.abi,
+        ItemManager.networks[networkId] && ItemManager.networks[networkId].address,
+      );
+      this.item = new this.web3.eth.Contract(
+        Item.abi,
+        Item.networks[networkId] && Item.networks[networkId].address,
       );
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({ loaded: true });
+
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -35,36 +35,57 @@ class App extends Component {
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
+  handleInputChange = (event) => {
+    const target = event.target;
+    const name = target.name;
+    const value =
+      target.type === "checkbox" ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  };
 
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
+  handleSubmit = async() => {
+    const { cost, itemName } = this.state;
+    console.log(itemName, cost, this.itemManager);
+    let result = await this.ItemManager.methods.createItem(itemName, cost).send({ from: this.accounts[0] });
+    console.log(result);
+    alert(
+      `Send ${cost}Wei to ${+result.events.SupplyChainStep.returnValues._address}`
+    );
   };
 
   render() {
-    if (!this.state.web3) {
+    if (!this.state.loaded) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <h1>Simply Payment / Supply Chain Example</h1>
+        <h2>Items</h2>
+        <h2>Add Items</h2>
+        <div>
+          <span>Cost in Wei: </span>
+          <input
+            type="text"
+            name="cost"
+            value={ this.state.cost }
+            onChange={ this.handleInputChange }
+          />
+        </div>
+        <div>
+          <span>Item Identifier: </span>
+          <input
+            type="text"
+            name="itemName"
+            value={ this.state.itemName }
+            onChange={ this.handleInputChange }
+          />
+        </div>
+        <button
+          type="button"
+          onClick={ this.handleSubmit }
+        >Create New Item</button>
       </div>
     );
   }
